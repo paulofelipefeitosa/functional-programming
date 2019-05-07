65e2a6835a9173c9114eefc4836b82f07f331452
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-05-06 23:38:02 -0300

    Update Multiset typeclass to lazily evaluate Multiset concrete element type

--- Multiset.hs
+++ Multiset.hs
@@ -1,13 +1,13 @@
 module Multiset(Multiset(..))
     where
 
-class Multiset a where
-    insert :: (Eq elem, Ord elem) => elem -> a -> a
-    remove :: (Eq elem, Ord elem) => elem -> a -> a
-    search :: (Eq elem, Ord elem) => elem -> a -> Int
-    union :: a -> a -> a
-    intersection :: a -> a -> a
-    minus :: a -> a -> a
-    inclusion :: a -> a -> Bool
-    sum :: a -> a -> a
-    size :: a -> Int
+class Multiset bag where
+    insert :: (Ord elem) => elem -> bag elem -> bag elem
+    remove :: (Ord elem) => elem -> bag elem -> bag elem
+    search :: (Ord elem) => elem -> bag elem -> Int
+    union :: (Ord elem) => bag elem -> bag elem -> bag elem
+    intersection :: (Ord elem) => bag elem -> bag elem -> bag elem
+    minus :: (Ord elem) => bag elem -> bag elem -> bag elem
+    inclusion :: (Ord elem) => bag elem -> bag elem -> Bool
+    sum :: (Ord elem) => bag elem -> bag elem -> bag elem
+    size :: (Ord elem) => bag elem -> Int
