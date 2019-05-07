4f955de8ae68e9c46cb58d1f7cec214065b804e0
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-05-06 23:38:39 -0300

    Add Multiset implementation to MultisetMap and List

--- MultisetList.hs
+++ MultisetList.hs
@@ -19,67 +19,67 @@ data MultisetList a = MultisetList [(a,Int)] deriving (Eq,Show)
 {-
  - Insere um elemento na estrutura. Caso o elemento ja existe, sua quantidade na estrutura sera incrementada.
  -}
-instance Multiset.Multiset (MultisetList a) where
+instance Multiset.Multiset MultisetList where
     insert elem (MultisetList []) = MultisetList [(elem, 1)]
     insert elem (MultisetList list) = MultisetList (List.insert (elem, xq+1) (List.delete (elem, xq) list))
         where
-            xq = search elem (MultisetList list)
+            xq = Multiset.search elem (MultisetList list)
 
 {-
 - Remove um elemento da estrutura, levando em consideracao a manipulacao de sua quantidade na estrutura. 
 - Caso a quantidade atinja 0 (ou menos), o elemento deve realmente ser removido da estrutura
 -}
-remove elem (MultisetList []) = MultisetList []
-remove elem (MultisetList list)
-    | xq <= 1 = MultisetList (List.delete (elem, xq) list)
-    | otherwise = MultisetList (List.insert (elem, xq-1) (List.delete (elem, xq) list))
-    where
-        xq = search elem (MultisetList list)
+    remove elem (MultisetList []) = MultisetList []
+    remove elem (MultisetList list)
+        | xq <= 1 = MultisetList (List.delete (elem, xq) list)
+        | otherwise = MultisetList (List.insert (elem, xq-1) (List.delete (elem, xq) list))
+        where
+            xq = Multiset.search elem (MultisetList list)
 
 {-
  - Busca um elemento na estrutura retornando sua quantidade. Caso o elemento nao exista, retorna 0 como a quantidade.
 -}
-search elem (MultisetList []) = 0
-search elem (MultisetList ((x,xq):xs))
-    | x == elem = xq
-    | otherwise = search elem (MultisetList xs)
+    search elem (MultisetList []) = 0
+    search elem (MultisetList ((x,xq):xs))
+        | x == elem = xq
+        | otherwise = Multiset.search elem (MultisetList xs)
 
 {-
  - Faz a uniao deste Bag com otherBag. A uniao consiste em ter os elementos dos dois Bags com suas maiores quantidades.
  - Por exemplo, A = {(a,1),(c,3)}, B = {(b,2),(c,1)}. A.union(B) deixa A = {(a,1),(c,3),(b,2)}
  -}
-union bag (MultisetList []) = bag
-union (MultisetList []) bag = bag
-union (MultisetList (hx:xs)) (MultisetList (hy:ys)) 
-    | x == y = if xq >= yq 
-        then mlConcat (MultisetList [(x,xq)]) (union (MultisetList xs) (MultisetList ys))
-        else mlConcat (MultisetList [(y,yq)]) (union (MultisetList xs) (MultisetList ys))
-    | x < y = mlConcat (MultisetList [(x,xq)]) (union (MultisetList xs) (MultisetList (hy:ys)))
-    | otherwise = mlConcat (MultisetList [(y,yq)]) (union (MultisetList (hx:xs)) (MultisetList ys))
-    where
-        x = fst hx
-        xq = snd hx
-        y = fst hy
-        yq = snd hy
+    union bag (MultisetList []) = bag
+    union (MultisetList []) bag = bag
+    union (MultisetList (hx:xs)) (MultisetList (hy:ys)) 
+        | x == y = if xq >= yq 
+            then mlConcat (MultisetList [(x,xq)]) (Multiset.union (MultisetList xs) (MultisetList ys))
+            else mlConcat (MultisetList [(y,yq)]) (Multiset.union (MultisetList xs) (MultisetList ys))
+        | x < y = mlConcat (MultisetList [(x,xq)]) (Multiset.union (MultisetList xs) (MultisetList (hy:ys)))
+        | otherwise = mlConcat (MultisetList [(y,yq)]) (Multiset.union (MultisetList (hx:xs)) (MultisetList ys))
+        where
+            x = fst hx
+            xq = snd hx
+            y = fst hy
+            yq = snd hy
 
 {-
  - Faz a intersecao deste Bag com otherBag. A intersecao consiste em ter os elementos que estao em ambos os bags com suas 
  - menores quantidades. Por exemplo, Seja A = {(a,3),(b,1)} e B = {(a,1)}. Assim, A.intersection(B) deixa A = {(a,1)}
  - Caso senhum elemento de A esteja contido em B ent�o a intersecao deixa A vazio.
  -}
-intersection (MultisetList []) bag = MultisetList []
-intersection bag (MultisetList []) = MultisetList []
-intersection (MultisetList (hx:xs)) (MultisetList (hy:ys))
-    | x == y = if xq <= yq 
-        then mlConcat (MultisetList [(x,xq)]) (intersection (MultisetList xs) (MultisetList ys))
-        else mlConcat (MultisetList [(y,yq)]) (intersection (MultisetList xs) (MultisetList ys))
-    | x < y = intersection (MultisetList xs) (MultisetList (hy:ys))
-    | otherwise = intersection (MultisetList (hx:xs)) (MultisetList ys)
-    where
-        x = fst hx
-        xq = snd hx
-        y = fst hy
-        yq = snd hy
+    intersection (MultisetList []) bag = MultisetList []
+    intersection bag (MultisetList []) = MultisetList []
+    intersection (MultisetList (hx:xs)) (MultisetList (hy:ys))
+        | x == y = if xq <= yq 
+            then mlConcat (MultisetList [(x,xq)]) (Multiset.intersection (MultisetList xs) (MultisetList ys))
+            else mlConcat (MultisetList [(y,yq)]) (Multiset.intersection (MultisetList xs) (MultisetList ys))
+        | x < y = Multiset.intersection (MultisetList xs) (MultisetList (hy:ys))
+        | otherwise = Multiset.intersection (MultisetList (hx:xs)) (MultisetList ys)
+        where
+            x = fst hx
+            xq = snd hx
+            y = fst hy
+            yq = snd hy
 
 {-
  - Faz a diferenca deste Bag com otherBag. A diferenca A \ B entre bags eh definida como segue:
@@ -88,40 +88,41 @@ intersection (MultisetList (hx:xs)) (MultisetList (hy:ys))
      Caso essa quantidade seja negativa o elemento deve serremovido do Bag. 
      Por exemplo, seja A = {(a,3),(b,1)} e B = {(b,2),(a,1)}. Assim, A.minus(B) deixa A = {(a,2)}.
 -}
-minus bag (MultisetList []) = bag
-minus (MultisetList []) bag = (MultisetList [])
-minus (MultisetList ((x,xq):xs)) (MultisetList ((y,yq):ys))
-    | x == y = if (yq >= xq) 
-        then minus (MultisetList xs) (MultisetList ys)
-        else mlConcat (MultisetList [(x,xq - yq)]) (minus (MultisetList xs) (MultisetList ys))
-    | x < y = mlConcat (MultisetList [(x,xq)]) (minus (MultisetList xs) (MultisetList ((y,yq):ys)))
-    | otherwise = minus (MultisetList ((x,xq):xs)) (MultisetList ys)
+    minus bag (MultisetList []) = bag
+    minus (MultisetList []) bag = (MultisetList [])
+    minus (MultisetList ((x,xq):xs)) (MultisetList ((y,yq):ys))
+        | x == y = if (yq >= xq) 
+            then Multiset.minus (MultisetList xs) (MultisetList ys)
+            else mlConcat (MultisetList [(x,xq - yq)]) (Multiset.minus (MultisetList xs) (MultisetList ys))
+        | x < y = mlConcat (MultisetList [(x,xq)]) (Multiset.minus (MultisetList xs) (MultisetList ((y,yq):ys)))
+        | otherwise = Multiset.minus (MultisetList ((x,xq):xs)) (MultisetList ys)
 
 {-
  - Testa se este Bag esta incluso em otherBag. Para todo elemento deste bag, sua quantidade
  - deve ser menor or igual a sua quantidade em otherBag.
 -}
-inclusion (MultisetList []) bag = True
-inclusion bag (MultisetList []) = False
-inclusion (MultisetList ((x,xq):xs)) (MultisetList ((y,yq):ys))
-    | x == y = if xq <= yq then inclusion (MultisetList xs) (MultisetList ys) else False
-    | x < y = False
-    | otherwise = inclusion (MultisetList ((x,xq):xs)) (MultisetList ys)
+    inclusion (MultisetList []) bag = True
+    inclusion bag (MultisetList []) = False
+    inclusion (MultisetList ((x,xq):xs)) (MultisetList ((y,yq):ys))
+        | x == y = if xq <= yq then Multiset.inclusion (MultisetList xs) (MultisetList ys) else False
+        | x < y = False
+        | otherwise = Multiset.inclusion (MultisetList ((x,xq):xs)) (MultisetList ys)
 
-mlConcat (MultisetList bag) (MultisetList otherBag) = MultisetList (bag ++ otherBag)
 
 {-
  - Realiza a soma deste Bag com otherBag. A soma de dois bags contem os elementos dos dois bags com suas quantidades somadas. 
  -}
-sum bag (MultisetList []) = bag
-sum (MultisetList []) bag = bag
-sum (MultisetList ((x,xq):xs)) (MultisetList ((y,yq):ys))
-    | x == y = mlConcat (MultisetList [(x,xq + yq)]) (sum (MultisetList xs) (MultisetList ys))
-    | x < y = mlConcat (MultisetList [(x,xq)]) (sum (MultisetList xs) (MultisetList ((y,yq):ys)))
-    | otherwise = mlConcat (MultisetList [(y,yq)]) (sum (MultisetList ((x,xq):xs)) (MultisetList ys))
+    sum bag (MultisetList []) = bag
+    sum (MultisetList []) bag = bag
+    sum (MultisetList ((x,xq):xs)) (MultisetList ((y,yq):ys))
+        | x == y = mlConcat (MultisetList [(x,xq + yq)]) (Multiset.sum (MultisetList xs) (MultisetList ys))
+        | x < y = mlConcat (MultisetList [(x,xq)]) (Multiset.sum (MultisetList xs) (MultisetList ((y,yq):ys)))
+        | otherwise = mlConcat (MultisetList [(y,yq)]) (Multiset.sum (MultisetList ((x,xq):xs)) (MultisetList ys))
 
 {-
  - Retorna a quantidade total de elementos no Bag
  -}
-size (MultisetList []) = 0
-size (MultisetList ((x,xq):xs)) = xq + (size (MultisetList xs))
+    size (MultisetList []) = 0
+    size (MultisetList ((x,xq):xs)) = xq + (Multiset.size (MultisetList xs))
+
+mlConcat (MultisetList bag) (MultisetList otherBag) = MultisetList (bag ++ otherBag)
--- MultisetMap.hs
+++ MultisetMap.hs
@@ -11,38 +11,40 @@ module MultisetMap ()
  - Eh recomendavel que voce consulte a documentacao de Data.Map
  -}
 import qualified Data.Map as Map
+import qualified Multiset as Multiset
 
 data MultisetMap a = MultisetMap (Map.Map a Int) deriving (Eq,Show)
 
+instance Multiset.Multiset MultisetMap where
 {-
  - Insere um elemento na estrutura. Caso o elemento ja existe, sua quantidade na estrutura sera incrementada.
  -}
-insert elem (MultisetMap map) = (MultisetMap (Map.insertWith (+) elem 1 map))
+    insert elem (MultisetMap map) = (MultisetMap (Map.insertWith (+) elem 1 map))
 
 {-
 - Remove um elemento da estrutura, levando em consideracao a manipulacao de sua quantidade na estrutura. 
 - Caso a quantidade atinja 0 (ou menos), o elemento deve realmente ser removido da estrutura
 -}
-remove elem (MultisetMap map) = let f k v = if v == 1 then Nothing else Just (v-1) 
-    in (MultisetMap (Map.updateWithKey f elem map))
+    remove elem (MultisetMap map) = let f k v = if v == 1 then Nothing else Just (v-1) 
+        in (MultisetMap (Map.updateWithKey f elem map))
 
 {-
  - Busca um elemento na estrutura retornando sua quantidade. Caso o elemento nao exista, retorna 0 como a quantidade.
 -}
-search elem (MultisetMap map) = Map.findWithDefault 0 elem map
+    search elem (MultisetMap map) = Map.findWithDefault 0 elem map
 
 {-
  - Faz a uniao deste Bag com otherBag. A uniao consiste em ter os elementos dos dois Bags com suas maiores quantidades.
  - Por exemplo, A = {(a,1),(c,3)}, B = {(b,2),(c,1)}. A.union(B) deixa A = {(a,1),(c,3),(b,2)}
 -}
-union (MultisetMap map) (MultisetMap otherMap) = (MultisetMap (Map.unionWith (max) map otherMap))
+    union (MultisetMap map) (MultisetMap otherMap) = (MultisetMap (Map.unionWith (max) map otherMap))
 
 {-
  - Faz a intersecao deste Bag com otherBag. A intersecao consiste em ter os elementos que estao em ambos os bags com suas 
  - menores quantidades. Por exemplo, Seja A = {(a,3),(b,1)} e B = {(a,1)}. Assim, A.intersection(B) deixa A = {(a,1)}
  - Caso senhum elemento de A esteja contido em B ent�o a intersecao deixa A vazio.
 -}
-intersection (MultisetMap map) (MultisetMap otherMap) = (MultisetMap (Map.intersectionWith (min) map otherMap))
+    intersection (MultisetMap map) (MultisetMap otherMap) = (MultisetMap (Map.intersectionWith (min) map otherMap))
 
 {-
  - Faz a diferenca deste Bag com otherBag. A diferenca A \ B entre bags eh definida como segue:
@@ -51,21 +53,21 @@ intersection (MultisetMap map) (MultisetMap otherMap) = (MultisetMap (Map.inters
      Caso essa quantidade seja negativa o elemento deve ser removido do Bag. 
      Por exemplo, seja A = {(a,3),(b,1)} e B = {(b,2),(a,1)}. Assim, A.minus(B) deixa A = {(a,2)}.
 -}
-minus (MultisetMap map) (MultisetMap otherMap) = let f vl vr = if vl > vr then Just (vl - vr) else Nothing 
-    in (MultisetMap (Map.differenceWith f map otherMap))
+    minus (MultisetMap map) (MultisetMap otherMap) = let f vl vr = if vl > vr then Just (vl - vr) else Nothing 
+        in (MultisetMap (Map.differenceWith f map otherMap))
 
 {-
  - Testa se este Bag esta incluso em otherBag. Para todo elemento deste bag, sua quantidade
  - deve ser menor or igual a sua quantidade em otherBag.
 -}
-inclusion (MultisetMap map) (MultisetMap otherMap) = otherMap == (Map.unionWith (max) map otherMap)
+    inclusion (MultisetMap map) (MultisetMap otherMap) = otherMap == (Map.unionWith (max) map otherMap)
 
 {-
  - Realiza a soma deste Bag com otherBag. A soma de dois bags contem os elementos dos dois bags com suas quantidades somadas. 
 -}
-sum (MultisetMap map) (MultisetMap otherMap) = (MultisetMap (Map.unionWith (+) map otherMap))
+    sum (MultisetMap map) (MultisetMap otherMap) = (MultisetMap (Map.unionWith (+) map otherMap))
 
 {-
  - Retorna a quantidade total de elementos no Bag
 -}
-size (MultisetMap map) = let f v acc = v + acc in (Map.foldr f 0 map)
+    size (MultisetMap map) = let f v acc = v + acc in (Map.foldr f 0 map)
